62cb27fe2fb27c1989fd56f654152d2b19e15d6c
Pramit <daspramit96@gmail.com>
2015-06-17 00:01:50 +0530

    fix int-long thing

--- src/MaxIndexProduct.java
+++ src/MaxIndexProduct.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Stack;
@@ -8,17 +7,18 @@ import java.util.Stack;
 public class MaxIndexProduct {
 
 
-	static List<Integer> inputList;
-
+	static List<Long> inputList;
+	static List<Long> inputListR;
 	public static void main(String[] args) {
 
 		Scanner in = null;
 		try {
-			inputList = new ArrayList<Integer>();
+			inputList = new ArrayList<Long>();
+			inputListR = new ArrayList<Long>();
 			in = new Scanner(System.in);
-			int n = in.nextInt();
-			for (int i=0; i<n; i++) {
-				inputList.add(in.nextInt());
+			long n = in.nextLong();
+			for (long i=0; i<n; i++) {
+				inputList.add(in.nextLong());
 			}
 			System.out.println(findMaxIndexProduct());
 		} finally {
@@ -26,38 +26,47 @@ public class MaxIndexProduct {
 		}
 	}
 
-	private static int findMaxIndexProduct() {
-		Stack<Integer> left = new Stack<Integer>();
-		Stack<Integer> right = new Stack<Integer>();
-		ArrayList<Integer> leftIndex = new ArrayList<Integer>();
-		ArrayList<Integer> rightIndex = new ArrayList<Integer>();
+	private static Long findMaxIndexProduct() {
+		Stack<Long> left = new Stack<Long>();
+		Stack<Long> right = new Stack<Long>();
+		ArrayList<Long> leftIndex = new ArrayList<Long>();
+		ArrayList<Long> rightIndex = new ArrayList<Long>();
 		//processing left region
-		int i=0;
-		for (i : inputList) {
-		while (left.peek() < inputList.get(i) && !left.isEmpty()) {
-			left.pop();
-		}
-		if (left.isEmpty()) {
-			leftIndex.add(0);
-		} else {
-			leftIndex.add(i);
-		}
+		for (long i=0; i<inputList.size(); i++) {
+			while (!left.isEmpty() && left.peek() <= inputList.get(i)) {
+				left.pop();
+			}
+			if (left.isEmpty()) {
+				leftIndex.add(0L);
+			} else {
+				leftIndex.add(inputList.indexOf(left.peek())+1);
+			}
 			left.push(inputList.get(i));
 		}
-			//processing right region
+		//processing right region
+		for (int j=inputList.size()-1; j >=0; j-- ) {
+			inputListR.add(inputList.get(j));
+		}
+		for (int j=0; j<inputListR.size(); j++) {
+			while ( !right.isEmpty() && right.peek() <= inputListR.get(j)) {
+				right.pop();
+			}
+			if (right.isEmpty()) {
+				rightIndex.add(0);
+			} else {
+				rightIndex.add(inputListR.size()-inputListR.indexOf(right.peek()));
+			}
+			right.push(inputListR.get(j));
+		}		
+
+		int maxIndex = 0;
+
+		for (int i=0; i<leftIndex.size() && i<rightIndex.size(); i++) {
+			if (maxIndex<leftIndex.get(i)*rightIndex.get(i)) {
+				maxIndex = leftIndex.get(i)*rightIndex.get(i);
+			}
+		}
 		
-			for (int j=i; j<inputList.size(); j++) {
-				while (left.peek() < inputList.get(j) && !left.isEmpty()) {
-					right.pop();
-				}
-				if (right.isEmpty()) {
-					rightIndex.add(0);
-				} else {
-					rightIndex.add(j);
-				}
-					right.push(inputList.get(j));
-					
-		return maxProduct;
-	}
+		return maxIndex;
 	}
 }
