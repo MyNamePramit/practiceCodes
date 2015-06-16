0cd13e1dce5646d12191e55723b7f8e9026ce176
Pramit <daspramit96@gmail.com>
2015-06-16 10:15:28 +0530

    complete this problem

--- src/MaxIndexProduct.java
+++ src/MaxIndexProduct.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Stack;
 
 
 public class MaxIndexProduct {
@@ -26,16 +27,37 @@ public class MaxIndexProduct {
 	}
 
 	private static int findMaxIndexProduct() {
-		for (int i : inputList) {
-			if (maxProduct < left*right)
-				maxProduct = left*right;
+		Stack<Integer> left = new Stack<Integer>();
+		Stack<Integer> right = new Stack<Integer>();
+		ArrayList<Integer> leftIndex = new ArrayList<Integer>();
+		ArrayList<Integer> rightIndex = new ArrayList<Integer>();
+		//processing left region
+		int i=0;
+		for (i : inputList) {
+		while (left.peek() < inputList.get(i) && !left.isEmpty()) {
+			left.pop();
 		}
+		if (left.isEmpty()) {
+			leftIndex.add(0);
+		} else {
+			leftIndex.add(i);
+		}
+			left.push(inputList.get(i));
+		}
+			//processing right region
+		
+			for (int j=i; j<inputList.size(); j++) {
+				while (left.peek() < inputList.get(j) && !left.isEmpty()) {
+					right.pop();
+				}
+				if (right.isEmpty()) {
+					rightIndex.add(0);
+				} else {
+					rightIndex.add(j);
+				}
+					right.push(inputList.get(j));
+					
 		return maxProduct;
 	}
-
-	private static int binarySearch(List<Integer> list, int start, int end, int key) {
-		
-		return 0;
 	}
-
 }
